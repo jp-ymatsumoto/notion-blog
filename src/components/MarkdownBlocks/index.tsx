0c747c5034436a565f3e0134b872c69d58027e74
jp-ymatsumoto <y.matsumoto@iwate-designer.com>
2023-06-09 00:51:35 +0900

    MarkdownBlocksコンポーネントを追加しました

---
+++ src/components/MarkdownBlocks/index.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+
+type Props = {
+  body: string
+}
+
+export const MarkdownBlocks = (props: Props) => {
+  return (
+    <ReactMarkdown
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || '')
+          return !inline && match ? (
+            <SyntaxHighlighter
+              {...props}
+              style={dark}
+              language={match[1]}
+              PreTag="div"
+            >
+              {String(children).replace(/\n$/, '')}
+            </SyntaxHighlighter>
+          ) : (
+            <code {...props} className={className}>
+              {children}
+            </code>
+          )
+        },
+      }}
+    >
+      {props.body}
+    </ReactMarkdown>
+  )
+}

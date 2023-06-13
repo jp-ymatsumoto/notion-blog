2436c586d546099d5bb1bedd76b1f4eb5bc4a0f7
jp-ymatsumoto <y.matsumoto@iwate-designer.com>
2023-06-13 22:32:24 +0900

    記事データの型にcoverとcategoryを追加しました

--- src/libs/notion.ts
+++ src/libs/notion.ts
@@ -2,8 +2,10 @@ import { Client } from '@notionhq/client'
 import { NotionToMarkdown } from 'notion-to-md'
 
 export type Post = {
+  cover: string | null
   title: string
   description: string
+  category: string
   tags: string[]
   slug: string
   createdAt: string
@@ -20,28 +22,35 @@ const notion = new Client({
 
 const n2m = new NotionToMarkdown({ notionClient: notion })
 
+const PAGE_SIZE = 2
+
+const queryObject = {
+  // 公開フラグがtrueのブログ記事だけ取得する
+  filterPublished: {
+    property: 'published',
+    checkbox: {
+      equals: true,
+    },
+  },
+  // 作成日時の降順で取得する
+  sortsCreatedAt: [
+    {
+      property: 'createdAt',
+      direction: 'descending' as 'descending' | 'ascending',
+    },
+  ],
+}
+
 // すべてのブログ記事を生データで取得する
 const getPostsRawData = async () => {
   return await notion.databases.query({
     database_id: process.env.NOTION_DATABASE_ID as string,
-    // 公開フラグがtrueのブログ記事だけ取得する
-    filter: {
-      property: 'published',
-      checkbox: {
-        equals: true,
-      },
-    },
-    // 作成日時の降順で取得する
-    sorts: [
-      {
-        property: 'createdAt',
-        direction: 'descending',
-      },
-    ],
+    filter: queryObject.filterPublished,
+    sorts: queryObject.sortsCreatedAt,
   })
 }
 
-// すべてのブログ記事を生データで取得する
+// 一覧ページのブログ記事を生データで取得する
 export const getPostsDataPagination = async (
   pageSize: number = 2,
   startCursor: string | undefined = undefined
@@ -50,22 +59,9 @@ export const getPostsDataPagination = async (
     database_id: process.env.NOTION_DATABASE_ID as string,
     page_size: pageSize,
     start_cursor: startCursor,
-    // 公開フラグがtrueのブログ記事だけ取得する
-    filter: {
-      property: 'published',
-      checkbox: {
-        equals: true,
-      },
-    },
-    // 作成日時の降順で取得する
-    sorts: [
-      {
-        property: 'createdAt',
-        direction: 'descending',
-      },
-    ],
+    filter: queryObject.filterPublished,
+    sorts: queryObject.sortsCreatedAt,
   })
-  console.log(rawPosts)
 
   const posts = rawPosts.results.map((post) => getPostProperties(post))
   return posts
@@ -90,8 +86,10 @@ const getPostRawData = async (slug: string) => {
 // ブログ記事の生データから必要なプロパティを抽出する
 const getPostProperties: (post: any) => Post = (post) => {
   return {
+    cover: post.cover === null ? null : post.cover.external.url,
     title: post.properties.title.title[0].plain_text,
     description: post.properties.description.rich_text[0].plain_text,
+    category: post.properties.category.select.name,
     tags: post.properties.tags.multi_select.map((tag: any) => tag.name),
     slug: post.properties.slug.formula.string,
     createdAt: post.properties.createdAt.created_time,
@@ -153,7 +151,6 @@ export const getTags = async () => {
 
 export const getSlugs = async () => {
   const rawPosts = await getPostsRawData()
-  console.log(rawPosts)
 
   const posts: { slug: string }[] = rawPosts.results.map((post) =>
     getPostPropertySlug(post)
